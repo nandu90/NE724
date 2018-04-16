8d88707373477466f6901aebb948954618062c42
Nadish Saini <nsaini3@ncsu.edu>
2018-04-16 18:54:16 -0400

    Deleted unnecessary folders

--- Project1/obj/generalFunc.d
+++
@@ -1,9 +0,0 @@
-obj/generalFunc.o: src/generalFunc.c include/generalFunc.h include/common.h \
- /usr/lib/openmpi/include/mpi.h \
- /usr/lib/openmpi/include/mpi_portable_platform.h include/input.h
-src/generalFunc.c:
-include/generalFunc.h:
-include/common.h:
-/usr/lib/openmpi/include/mpi.h:
-/usr/lib/openmpi/include/mpi_portable_platform.h:
-include/input.h:
