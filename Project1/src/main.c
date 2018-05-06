54af9a4bf23fa72266d48b64b736154d4547aae1
Nadish Saini <nsaini3@ncsu.edu>
2018-05-06 19:20:55 -0400

    time

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -361,7 +361,7 @@ int main(int argc, char **argv)
     
     //Time realted variables   
     deltat = 0.01/3600.0;
-    double totalTime = 5000.0/3600.0;
+    double totalTime = 10.0/3600.0;
     t = 0.0;
     double trip = 2.0/3600.0;
     double toperate = 365.0*24.0*60.0*60.0;   //Operating time in secs
